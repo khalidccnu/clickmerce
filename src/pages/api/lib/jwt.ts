bdb08182eca599d59ca7d0b5c97777ba343b7627
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2025-10-30 21:10:04 +0600

    jwt algorithm modified

--- src/pages/api/lib/jwt.ts
+++ src/pages/api/lib/jwt.ts
@@ -3,7 +3,7 @@ import { IToken } from '@modules/auth/lib/interfaces';
 import { sign, verify } from 'jsonwebtoken';
 
 export const jwtSign = (payload: { user: IToken['user'] }): string => {
-  return sign(payload, Env.supabaseJwtSecret, { algorithm: 'HS512', expiresIn: 60 * 60 * 24 * 7 });
+  return sign(payload, Env.supabaseJwtSecret, { algorithm: 'HS256', expiresIn: 60 * 60 * 24 * 7 });
 };
 
 export const jwtVerify = (token: string): IToken => {
